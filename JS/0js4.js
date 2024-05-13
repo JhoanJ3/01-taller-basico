df55658d02f17e6a62d229513b2dd844b75bb086
JhoanJ <jhoanf3001@gmail.com>
2024-05-13 12:43:31 -0500

    Ej03

---
+++ JS/0js4.js
@@ -0,0 +1,10 @@
+
+function sumar( numero1, numero2 ) {
+     numero1 = parseFloat (prompt ("Digita digita numero 1"));
+     numero2 = parseFloat (prompt ("Digita digita numero 2"));
+
+     let suma = numero1 + numero2
+     console.log (suma)
+    
+}
+sumar()
