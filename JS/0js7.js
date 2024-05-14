23d0c82ffe25e70e61ed85620a357c50c9e519f1
JhoanJ <jhoanf3001@gmail.com>
2024-05-13 20:29:37 -0500

    Ej07

---
+++ JS/0js7.js
@@ -0,0 +1,9 @@
+// Escribe un programa para la consola que le pida al usuario dos números e imprima en la consola los números en ese rango. Por ejemplo, si el usuario ingresa 5 como primer número y 10 como segundo número, el programa debe imprimir:
+
+
+let numeroI = parseInt (prompt("Ingrese un #")); 
+let numeroF = parseInt (prompt("Ingrese un #")); 
+
+    for ( let i = numeroI; i <= numeroF; i++ ) {
+        console.log( i );
+    }  
